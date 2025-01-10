2e869bab116d84d314f8fb31fa4708f333c65a4f
Vusile Terence Silonda <vusile@gmail.com>
2025-01-10 17:46:21 +0300

    Final touches

--- model/huduma.go
+++ model/huduma.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Huduma struct {
 	gorm.Model
-	ID          uint `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
-	Name        string
+	ID          uint   `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
+	Name        string `gorm:"uniqueIndex;size:40"`
 	SpecifyDays bool
 	Priority    int
 }
